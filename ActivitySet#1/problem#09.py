d314812ee3c5d37b224c480ee2c9da3e2eab4796
BM Prajwal <89903153+bmprajwal@users.noreply.github.com>
2022-05-18 06:36:07 +0000

    .

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,16 @@
 # Lists
 
-filename = "dataset/romeo.txt"
+filename = "romeo.txt"
+
+fname = input("Enter file name: ")
+fh = open(fname)
+
+roll = list()
+for line in fh :
+    words = line.split()
+    for word in words :
+        if word not in roll:
+            roll.append(word)
+
+roll.sort()
+print(roll)
