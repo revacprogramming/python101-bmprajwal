984168c1f6aa847d3c5bbcf3cbab5296bd87aa02
BM Prajwal <89903153+bmprajwal@users.noreply.github.com>
2022-05-21 06:27:02 +0000

    dict

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,23 @@
 # Dictionaries
 
-filename = "dataset/mbox-short.txt"
+filename = 'mbox-short.txt'
+fhand = open(filename)
+
+dick = dict()
+
+for line in fhand :
+    if line.startswith('From:') : continue
+    if not line.startswith('From') : continue
+
+    word = line.split()
+    dick[word[1]] = dick.get(word[1], 0) + 1
+
+maxkey = None
+maxval = None
+
+for key, value in dick.items() : 
+    if maxval is None or maxval < value:
+        maxval = value
+        maxkey = key
+
+print(maxkey, maxval)
