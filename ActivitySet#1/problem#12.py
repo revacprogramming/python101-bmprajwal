1ad63478277cebd92981c7aedc07529811cb0605
BM Prajwal <89903153+bmprajwal@users.noreply.github.com>
2022-05-25 09:38:28 +0000

    .

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,26 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+
+import re
+
+# filename = 'regex_sum_1548401.txt'
+# fhand = open(filename, "r")
+
+# lst = list()
+
+# for line in fhand :
+#   line = line.rstrip()
+#   ls = re.findall('([0-9]+)', line)
+#   # lst.append(ls)
+#   lst += ls
+
+# # la = [int(i) for i in lst]
+
+# sum = 0
+# for i in lst :
+#   sum += int(i)
+
+# # for i in la:
+# #   sum += i
+# print(sum)
+
