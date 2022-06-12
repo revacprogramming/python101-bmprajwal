c469ecc60ad7a197cefe70136d19206712d97280
BM Prajwal <89903153+bmprajwal@users.noreply.github.com>
2022-06-12 05:52:59 +0000

    .

--- ActivitySet#2/problem#8.py
+++ ActivitySet#2/problem#8.py
@@ -1,7 +1,11 @@
 
 
-class Menu:
-    """fill in class definition here"""
+class Menu(dict):
+    def __setitem__(self, key='',value=0):
+        self.__dict__[key] = value
+
+    def __str__(self):
+        return ('\n'.join([str(i + ' ' + str(j)) for i, j in self.__dict__.items()]))
 
 
 m = Menu()
