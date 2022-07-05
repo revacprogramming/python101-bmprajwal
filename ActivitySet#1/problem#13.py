d8207dfe77dbc4c0ad72ebf862d020b2993d3e25
BM Prajwal <89903153+bmprajwal@users.noreply.github.com>
2022-07-05 20:28:38 +0530

    Update problem#13.py

--- ActivitySet#1/problem#13.py
+++ ActivitySet#1/problem#13.py
@@ -1,22 +1,12 @@
-# Network Programming
-# https://www.py4e.com/lessons/network
 import socket
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-url=input('enter the url- ')
-counts=int(input('enter count: '))
-position=int(input('enter position: '))
-for i in range(counts):
-    count=0
-    html=urlopen(url).read()
-    soup=BeautifulSoup(html,'html.parser')
-    # print(count)
-    tags = soup('a')
-    for tag in tags:
-        if count<position:
-            # print(tag.get('href'))
-            count+=1
-            url=tag.get('href')
-    print('Retrieving:',url) 
-###done  .....
-......
+mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock.connect(('data.pr4e.org',80))
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
+mysock.send(cmd)
+
+while True:
+  data = mysock.recv(512)
+  if (len(data)<1):
+    break
+    print(data.decode())
+mysock.close()
