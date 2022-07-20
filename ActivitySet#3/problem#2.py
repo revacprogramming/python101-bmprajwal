49dcc77e3ceed966f7be800cd0ba089269585abc
BM Prajwal <89903153+bmprajwal@users.noreply.github.com>
2022-07-20 10:20:35 +0000

    .

--- ActivitySet#3/problem#2.py
+++ ActivitySet#3/problem#2.py
@@ -1,1 +1,15 @@
-....
+from fractions import Fraction as f
+n=int(input())
+d={}
+for i in range(n):
+  f1=0
+  s=''
+  m=int(input())
+  l=input().split()
+  for j in range(m):
+      f1+=f(f'1/{l[j]}')
+  for k in l:
+    s+=f'1/{k} + '
+  d[s[:-3]]=f1
+for a,b in d.items():
+  print(f'{a} = {b}')
